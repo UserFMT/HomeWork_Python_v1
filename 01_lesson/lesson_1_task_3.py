debba83d399710649fe99a5e936a96424317029e
Ev972 <Zhuzha972@gmail.com>
2024-08-30 18:55:28 +0700

    Добавление файлов

---
+++ 01_lesson/lesson_1_task_3.py
@@ -0,0 +1,3 @@
+first_name = input('Ваше имя - ')
+last_name = input('Ваше отчество - ')
+print('Вас зовут: ' + last_name+' '+ first_name)
---
+++ 01_lesson/lesson_1_task_5.py
@@ -0,0 +1,15 @@
+def print_numr(num):
+    print(num, end='')
+
+#88005553535
+print_numr('8')
+print_numr('8')
+print_numr('0')
+print_numr('0')
+print_numr('5')
+print_numr('5')
+print_numr('5')
+print_numr('3')
+print_numr('5')
+print_numr('3')
+print_numr('5')
