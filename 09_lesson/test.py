f37d0ee6e2610732dc2b22211afc668dd9094224
Ev972 <Zhuzha972@gmail.com>
2024-11-03 11:59:05 +0700

    ver.2

--- 09_lesson/test.py
+++ 09_lesson/test.py
@@ -6,21 +6,14 @@ db_connection_string = "postgresql://postgres:vfhn2010@localhost:5432/HW4"
 
 
 def test_db():
-    db = create_engine(db_connection_string)
+    db = Object(db_connection_string)
     # выбираем название всех таблиц из базы
-    insp = inspect(db).get_table_names()
+    insp = db.get_table_names()
 
-    """ добавить студента
-     1. определение уникального user_id
-     2. добавить student для  уникального user_id
-     3  удалить студента 
-     """
+    #Получаем id для тестирования
+    new_id_user = int(db.get_new_id()[0])
 
-    sql_statement = text("select max(user_id)+1  from student s")
-    new_id_user = db.connect().execute(sql_statement).first()
-    sql_statement = text(f"insert into users (user_id,user_email,subject_id) "
-                         f"values ({int(new_id_user[0])},'test_9999@com',10)")
-    db.connect().execute(sql_statement)
-    #db.connect().close()
-    assert 1==1
+    #Создаем user
+    db.insert_user(new_id_user,"Ошибка создания user")
 
+    assert 1 == 1
