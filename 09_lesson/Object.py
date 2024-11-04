40debc7b9b38c8c81fa5b88af5478112a8ceee29
Ev972 <Zhuzha972@gmail.com>
2024-11-04 10:20:39 +0700

    Корректировка по flake8 и добавление close()

--- 09_lesson/Object.py
+++ 09_lesson/Object.py
@@ -1,41 +1,41 @@
-from sqlalchemy import inspect, create_engine
+from sqlalchemy import create_engine
 from sqlalchemy.sql import text
+# from sqlalchemy import inspect
+
 
 class Object:
 
     __script = {
-        "get_id":"select max(user_id)+1 from student s",
+        "get_id": "select max(user_id)+1 from student s",
         "insert_user": "insert into users (user_id, user_email, subject_id) "
-                       "values ( :new_id, 'test_9999@com', 10)",
-        "insert_student":"",
-        "delete_student":"",
-        "delete_user":""
+                       "values ( %new_id, 'test_9999@com', 10)",
+        "insert_student": "",
+        "delete_student": "",
+        "delete_user": ""
     }
 
     def __init__(self, connection_string):
         self.__db = create_engine(connection_string)
 
-
     def get_table_names(self):
-        return self.__db.connect().dialect.get_table_names(self.__db.connect())
-
+        return (self.__db.connect().
+                dialect.get_table_names(self.__db.connect()))
 
     def connect(self):
         self.__db.connect()
         pass
 
-
     def get_new_id(self):
-        return self.__db.connect().execute(text(self.__script["get_id"])).first()
+        return (self.__db.connect().
+                execute(text(self.__script["get_id"])).first())
 
-
-    def insert_user(self, new_id, error):
+    def insert_user(self, new_id):
         self.__db.connect().begin()
         try:
             (self.__db.connect().
-             execute(text(self.__script["insert_user"], new_id = new_id)))
+             execute(text(self.__script["insert_user"]), {"new_id": new_id}))
             self.__db.connect().commit()
-        except Exception as e:
-            # случае ошибки откатываем транзакцию
-             self.__db.connect().rollback()
-             print(f"Ошибка: {error}")
+        except Exception as err:
+            # В случае ошибки откатываем транзакцию
+            self.__db.connect().rollback()
+            print(f"Ошибка: {err}")
